d0cc88a9ae6aa15c9f6f151886463234dca06911
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-02-15 18:48:12 +0100

    + Tutorial Panels

--- Assets/SCRIPTS/COMPONENTS/Tutorial/Tutorial.cs
+++ Assets/SCRIPTS/COMPONENTS/Tutorial/Tutorial.cs
@@ -1,16 +1,21 @@
 using GOM.Components.Bees;
 using GOM.Components.Player;
 using GOM.Components.Core;
+using GOM.Components.Workplaces;
+using System.Collections;
 using UnityEngine;
 
 namespace GOM.Components.Tutorial {
     public class Tutorial : MonoBehaviour {
-        [SerializeField] BeeComponent FirstBee;
-        [SerializeField] GameObject Panel01;
+        [SerializeField] private BeeComponent FirstBee;
+        [SerializeField] private GameObject Panel01;
+        [SerializeField] private Workplace TutorialWorkplace;
+        [SerializeField] private GameObject Button01;
         private GameManager _gameManager; 
         private PlayerManager _player;
 
         private bool _aux;
+        private bool _aux2 = false;
 
         private void Start() {
             _gameManager = GameManager.Instance;
@@ -20,6 +25,12 @@ namespace GOM.Components.Tutorial {
         }
 
         private void Update() {
+
+            if(TutorialWorkplace.HaveBee() && _aux2 == true) {
+                Button01.SetActive(true);
+                _aux2 = false;
+            }
+
             if(_player.GetMissNumber() == 0 || _aux) return;
 
             Panel01.SetActive(true);
@@ -30,6 +41,18 @@ namespace GOM.Components.Tutorial {
 
         public void OnResumeGameButton() {
             _gameManager.SetGameState(GameState.Playing);
+        }
+
+        public void ToggleAux() {
+            _aux2 = !_aux2;
+        }
+
+        private IEnumerator WaitAction() {
+             {
+                yield return null;
+            }
+
+            
         }
     }
 }
